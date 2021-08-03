18d22bbddc99bbc9f6490c028af6ac2b0600f798
migarabhanu <migarabhanu@gmail.com>
2021-08-03 07:50:53 +0530

    Initial Commit is adding

---
+++ main.py
@@ -0,0 +1,13 @@
+#CODE by Migara Bhanu
+#For Complete C04 Task in BITS'21 Intra-School Competition
+
+print("Hello This task is for ACICTS BITS'21 Competition")
+print("")
+ask=input("Do you know BITS is the best ever intra-based competition in Sri Lanka ? (y/n) ")
+if ask=="y":
+    print("")
+    print("Okay, Nice If you are an Anandian, You can participate too ! ")
+    input("Good Bye !")
+else:
+    print("")
+    input("Oh You better exit here ! ")
